7d41ccec09a6077295a536bff2a3ec68b44d6492
Shubham Kumar <shubhamkgr06@gmail.com>
2026-03-21 10:15:26 +0530

    Update 9_removeDuplicatesFromSortedList.cpp

--- (4). Linked List/9_removeDuplicatesFromSortedList.cpp
+++ (4). Linked List/9_removeDuplicatesFromSortedList.cpp
@@ -16,7 +16,7 @@ class Node {
 
 class Solution {
   public:
-    // Function to remove duplicates from sorted linked list.
+    //Question-(1): Function to remove duplicates from sorted linked list.
 
     //Method-1
     Node* removeDuplicates(Node* head) {
@@ -74,7 +74,7 @@ class Solution {
     }
 };
 
-//Agar keval diye gaye value wala nodes delete karna ho to
+//Question-(2): Agar keval diye gaye value wala nodes delete karna ho to
 
 Node* removeElements(Node* head, int val) {
     Node* dummy = new Node(-1);
@@ -107,3 +107,31 @@ while (head && head->data == val) {
 
 }
 
+//Question-(3): Agar list unsorted ho to duplicate remove ke liye  hum (dummy node + set ka use karte hai)
+class Solution {
+  public:
+    Node *removeDuplicates(Node *head) {
+        // your code goes here
+        unordered_set<int>st;//Har node ka address unique hota hai issliye value insert karo
+        Node* dummy=new Node(-1);
+        Node* res=dummy;
+        
+        Node* curr=head;
+        while(curr!=NULL){
+            
+            if(st.find(curr->data) != st.end()){
+                 curr=curr->next;// skip duplicate
+            }
+            else{
+                res->next=curr;
+                res=res->next; //move res
+                st.insert(curr->data);//insert correct value
+                curr=curr->next;
+                
+            }
+        }
+        
+        res->next = NULL;
+        return dummy->next;
+    }
+};
