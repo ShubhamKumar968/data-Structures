be5dc5a8f701465c44202d1ee3912bea5c1a1802
Shubham Kumar <shubhamkgr06@gmail.com>
2026-05-08 12:06:37 +0530

    Update 04_maximumSumOfNonAdjacentElements.cpp

--- (10). Dynamic programming/(1). 1-D  Dp/04_maximumSumOfNonAdjacentElements.cpp
+++ (10). Dynamic programming/(1). 1-D  Dp/04_maximumSumOfNonAdjacentElements.cpp
@@ -5,26 +5,34 @@ using namespace std;
 class Solution {
   public:
     // calculate the maximum sum with out adjacent
-    
-    //Method-01 : Recursion + memoization
-    vector<int>t;
-    int memo(vector<int>& arr, int n,int idx){
-        
+
+     //Method-01: Using forward indexing
+    int t[100001];
+    int solve(vector<int>& arr,int idx, int n){
         if(idx>=n){
             return 0;
         }
-        
-        if(t[idx] !=-1){
-            return t[idx];
-        }
-        
-        int take= arr[idx] + memo(arr,n,idx+2);
-        int skip=memo(arr,n,idx+1);
+        if(t[idx]!=-1) return t[idx];
+        int take=arr[idx]+solve(arr,idx+2,n);
+        int skip=solve(arr,idx+1,n);
         
         return t[idx]=max(take,skip);
     }
     
-    //Method-02: Bottom up
+    //Method-02: Using Backward indexing
+    int memo(vector<int>& arr, int n){
+        if(n<0){
+            return 0;
+        }
+        if(t[n]!=-1) return t[n];
+        
+        int take=arr[n]+memo(arr,n-2);
+        int skip=memo(arr,n-1);
+        
+        return t[n]=max(take,skip);
+    }
+    
+    //Method-03: Bottom up
     int bottomUp(vector<int>& arr, int n){
         // dp[i] represents the maximum sum we can obtain from subarray [0...i]
         vector<int>dp(n,0);
@@ -44,8 +52,10 @@ class Solution {
     }
     int findMaxSum(vector<int>& arr, int n) {
         // code here
-        t.resize(n,-1);
-        //return memo(arr,n,0);
+        int n=arr.size();
+        memset(t,-1,sizeof(t));
+        //return solve(arr,0,n);
+        //return memo(arr,n-1);
         return bottomUp(arr,n);
     }
 };
