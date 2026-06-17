f80f986196e46b0fc1589087bd381053433198fd
Shubham Kumar <shubhamkgr06@gmail.com>
2026-06-17 07:46:38 +0530

    Update 15_minimumHeightRoots.cpp

--- Graph Basics Problem/15_minimumHeightRoots.cpp
+++ Graph Basics Problem/15_minimumHeightRoots.cpp
@@ -4,64 +4,62 @@ using namespace std;
 
 class Solution {
   public:
-  
-    vector<int> minHeightRoot(int V, vector<vector<int>>& edges) {
-        // Code here
-        
-        vector<vector<int>> adj(V);
-        vector<int> deg(V, 0);
-        
-        // Step 1: Build undirected graph
-        for (auto &edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-            
-            deg[u]++;
-            deg[v]++;
-        }
-        
-        // Step 2: Initialize queue with all leaves(leaf node)
+    //Use Kahn's alforithm for undirected graph
+    void bfs(int V, vector<vector<int>>& adj, vector<int>& deg, vector<int>& res) {
         queue<int> q;
+        
+        // Find initial leaves
         for (int i = 0; i < V; i++) {
-            if (deg[i] == 1) {//leaf node ka degree=1
-                q.push(i);
-            }
+            if (deg[i] == 1) q.push(i);
         }
         
-        int remainingNodes = V;
-        
-        // Step 3: Trim leaves layer by layer
-        while (remainingNodes > 2) {
-            
-            int size = q.size();
-            remainingNodes -= size;
-            
-            while (size--) {
-                int u = q.front();
+        // Peel layers until 2 or fewer nodes remain total
+        while (V > 2) {
+            int n = q.size();
+            V -= n; 
+            while (n--) {
+                int curr = q.front();
                 q.pop();
                 
-                for (auto &v : adj[u]) {
-                    deg[v]--;
-                    if (deg[v] == 1) {
-                        q.push(v);
+                for (auto& nbr : adj[curr]) {
+                    deg[nbr]--;
+                    if (deg[nbr] == 1) {
+                        q.push(nbr);
                     }
                 }
             }
         }
         
-        // Step 4: Remaining nodes are answer
-        vector<int> res;
+        // Collect the final 1 or 2 center nodes
         while (!q.empty()) {
             res.push_back(q.front());
             q.pop();
         }
+    }
+
+    vector<int> minHeightRoot(int V, vector<vector<int>>& edges) {
+        // Edge cases: If 1 or 2 nodes, they are automatically the answer
+        if (V == 1) return {0};
+        if (V == 2) return {0, 1};
         
+        vector<vector<int>> adj(V);
+        vector<int> deg(V, 0);
+        
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+            
+            adj[u].push_back(v); 
+            adj[v].push_back(u);
+            deg[u]++;
+            deg[v]++;
+        }
+        
+        vector<int> res;
+        bfs(V, adj, deg, res);
         return res;
     }
 };
-//“Minimum height trees are rooted at the center(s) of the tree, 
-//which can be found by trimming leaves layer by layer.”
+//“Minimum height trees are rooted er.at the center(s) of the tree, 
+//which can be found by trimming leaves layer by lay”
 
