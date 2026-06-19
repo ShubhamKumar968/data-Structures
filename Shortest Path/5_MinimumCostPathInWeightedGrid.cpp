d6fe1200b94ab68451332a671fd75a37a7586024
Shubham Kumar <shubhamkgr06@gmail.com>
2026-06-19 09:01:00 +0530

    Update 5_MinimumCostPathInWeightedGrid.cpp

--- Shortest Path/5_MinimumCostPathInWeightedGrid.cpp
+++ Shortest Path/5_MinimumCostPathInWeightedGrid.cpp
@@ -4,48 +4,7 @@ using namespace std;
 
 class Solution {
   public:
-    // Function to return the minimum cost to react at bottom
-    // right cell from top left cell.
     
-    //Method-1 BFS(TLE) Tc=O(m*n) but fails
-    vector<vector<int>>dirs={{1,0},{0,1},{-1,0},{0,-1}};
-    typedef pair<int,int>p;
-    int bfs(vector<vector<int>>& grid){
-        
-        int m=grid.size();
-        int n=grid[0].size();
-        
-        queue<pair<int,p>>q;
-        vector<vector<int>>dist(m,vector<int>(n,INT_MAX));
-        dist[0][0]=grid[0][0];
-        q.push({grid[0][0],{0,0}});
-        
-        while(!q.empty()){
-            
-            auto curr=q.front();
-            q.pop();
-            
-            //auto [x,y]=curr.second;
-            p cord=curr.second;
-            int x=cord.first,y=cord.second;
-            int d=curr.first;
-            
-            for(auto &dir:dirs){
-                int nx=x+dir[0];
-                int ny=y+dir[1];
-                
-                if(nx>=0 && nx<m && ny>=0 && ny<n ){
-                    
-                    if( dist[nx][ny] > (d + grid[nx][ny]) ){
-                        
-                        dist[nx][ny]=d + grid[nx][ny];
-                        q.push({d + grid[nx][ny],{nx,ny}});
-                    }
-                }
-            }
-        }
-        return dist[m-1][n-1];
-    }
 
     //Method-2(Dijkstra) optimal (TC= O(m*n log(m*n)))
     vector<vector<int>>dirs={{1,0},{0,1},{-1,0},{0,-1}};
