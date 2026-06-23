c57cbc85b3f477a318ed0d4550ea6dc297c7a15e
Shubham Kumar <shubhamkgr06@gmail.com>
2026-06-23 14:00:27 +0530

    Update 10_StonglyConnectedComponent_UsingTarjanAlgorithm.cpp

--- Most Asked Graph Algorithms/10_StonglyConnectedComponent_UsingTarjanAlgorithm.cpp
+++ Most Asked Graph Algorithms/10_StonglyConnectedComponent_UsingTarjanAlgorithm.cpp
@@ -7,69 +7,66 @@ class Solution {
     //Why Tarjan over Kosaraju?
     //Ans:-No need to reverse graph; Single DFS; More space efficient
     //TC=O(V+E); SC=O(V)
-    void findSCC(int node, vector<int> adj[], vector<int>& disc, vector<int>& low, 
-        vector<bool>&inStack, stack<int>&st ,int& timer,vector<vector<int>>&allSCC,int& sccCount){
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& disc, vector<int>& low,
+             vector<bool>& inStack, stack<int>& st, int& timer, vector<vector<int>>& sccs) {
         
-        // 1. Initialize discovery and low-link values
         disc[node] = low[node] = timer++;
         st.push(node);
         inStack[node] = true;
 
-        for(auto &nbr : adj[node]) {
-            
-           if(disc[nbr] == -1) { // Not visited
-           
-                findSCC(nbr, adj, disc, low, inStack, st, timer, allSCC,sccCount);
-                // On backtrack, update low value from child
+        for (int nbr : adj[node]) {
+            if (disc[nbr] == -1) {
+                // Neighbor is unvisited: Recurse
+                dfs(nbr, adj, disc, low, inStack, st, timer, sccs);
+                
+                // On tree unraveling, update low value of the current node
                 low[node] = min(low[node], low[nbr]);
             } 
-            else if(inStack[nbr]==true) {
-                // Back-edge:  nbr is in the current recursion stack
-                // This means nbr is part of the current SCC we are exploring
+            else if (inStack[nbr]) {
+                // Back-edge/Cross-edge to a node currently in the SCC stack
                 low[node] = min(low[node], disc[nbr]);
             }
         }
 
-        // 2.If node  is the "head" of an SCC
-        if(disc[node] == low[node]) {
-            
-            vector<int> component;//we can skip it if we have to count only
-            
-            while(true) {
-                int topNode = st.top();
+        // If current node is the root/head of an SCC
+        if (low[node] == disc[node]) {
+            vector<int> component;
+            while (true) {
+                int curr = st.top();
                 st.pop();
-                inStack[topNode] = false;
-                
-                component.push_back(topNode);//we can skip it if we have to count only
-                
-                if(topNode == node) break;
+                inStack[curr] = false;
+                component.push_back(curr);
+                if (curr == node) break;
             }
-        
+            
+            // Requirement: Sort vertices inside the individual component
             sort(component.begin(), component.end());
-            allSCC.push_back(component);
-            sccCount++;//only counting
-        }
+            sccs.push_back(component);
+          
+            sccCount++//if we only have to count the scc;
+        } 
     }
 
-public:
-    vector<vector<int>> tarjans(int V, vector<int> adj[]) {
-        // code here
-        vector<int> disc(V, -1);
+    vector<vector<int>> tarjansSCC(int V, vector<vector<int>>& adj) {
+      
+        vector<int> disc(V, -1); // -1 marks unvisited nodes
         vector<int> low(V, -1);
         vector<bool> inStack(V, false);
         stack<int> st;
-        
-        vector<vector<int>>allSCC;
         int timer = 0;
-        int sccCount=0;
         
-        for(int i = 0; i < V; i++) {
-            if(disc[i] == -1) {
-                findSCC(i, adj, disc, low, inStack, st, timer,allSCC,sccCount);
+        vector<vector<int>> sccs;
+
+        // Run DFS for all unvisited nodes to handle disconnected components
+        for (int i = 0; i < V; i++) {
+            if (disc[i] == -1) {
+                dfs(i, adj, disc, low, inStack, st, timer, sccs);
             }
         }
-        // SCCs to be sorted lexicographically
-        sort(allSCC.begin(), allSCC.end());
-        return allSCC;
+
+        // Requirement: Sort the complete list of components lexicographically
+        sort(sccs.begin(), sccs.end());
+
+        return sccs;
     }
 };
