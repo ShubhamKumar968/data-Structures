9c66ea4a890a505703a32b49f425b81481f7d425
Shubham Kumar <shubhamkgr06@gmail.com>
2026-06-29 14:33:40 +0530

    Update 21_maximumFruitsIntoTheBaskets.cpp

--- (5). Sliding Window/21_maximumFruitsIntoTheBaskets.cpp
+++ (5). Sliding Window/21_maximumFruitsIntoTheBaskets.cpp
@@ -6,25 +6,25 @@ using namespace std;
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int n=fruits.size();
-        unordered_map<int,int>mp;
-        int i=0,j=0,cnt=0;
 
+        int n=fruits.size();
+        int cnt=0;
+        unordered_map<int,int>mp;
+
+        int i=0,j=0;
         while(j<n){
 
             mp[fruits[j]]++;
-            
-            while(mp.size()>2){
+
+            while (mp.size() > 2) {
 
                 mp[fruits[i]]--;
-                if(mp[fruits[i]]==0) mp.erase(fruits[i]);
-                i++;
-               
-            }
-            if(mp.size()<=2){
-                cnt=max(cnt,j-i+1);
-            }
-            
+                if (mp[fruits[i]] == 0) {
+                    mp.erase(fruits[i]);
+                }
+                i++; // Slide left boundary forward
+            } 
+            cnt = max(cnt, j - i + 1);
             j++;
         }
         return cnt;
