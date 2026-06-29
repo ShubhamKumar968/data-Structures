e32bac4364011792f5664054828ef2a3f0ecbe55
Shubham Kumar <shubhamkgr06@gmail.com>
2026-06-29 13:41:55 +0530

    Update 10_maximumSumOfDistinctSubarrayOfSizeK.cpp

--- (5). Sliding Window/10_maximumSumOfDistinctSubarrayOfSizeK.cpp
+++ (5). Sliding Window/10_maximumSumOfDistinctSubarrayOfSizeK.cpp
@@ -4,30 +4,39 @@ using namespace std;
 
 class Solution {
 public:
+  //Optimal Approach
+
+    typedef long long ll;
     long long maximumSubarraySum(vector<int>& arr, int k) {
-        //Optimal approach
+        
         int n=arr.size();
-        long long sum=0;
-        long long maxSum=0;
         unordered_map<int,int>mp;
+        ll res=0;
+        ll sum=0;
         int i=0,j=0;
+
         while(j<n){
+            
             sum+=arr[j];
             mp[arr[j]]++;
 
-            while(mp[arr[j]]>1){
-                mp[arr[i]]--;
-                sum-=arr[i];
-                i++;
-            }
             if(j-i+1==k){
-                maxSum=max(maxSum,sum);
-                mp[arr[i]]--;
+
+                if(mp.size()==k){
+                    res=max(res,sum);
+                }
+
                 sum-=arr[i];
+                mp[arr[i]]--;
+                if(mp[arr[i]]==0){
+                    mp.erase(arr[i]);
+                }
+
                 i++;
             }
+
             j++;
         }
-        return maxSum;
+        return res;
     }
 };
