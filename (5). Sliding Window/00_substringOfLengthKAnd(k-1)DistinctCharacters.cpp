55f568790c44d8e958b02a4b1acfc47eb973413e
Shubham Kumar <shubhamkgr06@gmail.com>
2026-06-29 13:54:25 +0530

    Update 00_substringOfLengthKAnd(k-1)DistinctCharacters.cpp

--- (5). Sliding Window/00_substringOfLengthKAnd(k-1)DistinctCharacters.cpp
+++ (5). Sliding Window/00_substringOfLengthKAnd(k-1)DistinctCharacters.cpp
@@ -6,32 +6,31 @@ using namespace std;
 class Solution {
   public:
     int substrCount(string &s, int k) {
-        // code here
+        
         int n=s.length();
-        int cnt=0;
         unordered_map<char,int>mp;
+        int cnt=0;
+        
         int i=0,j=0;
         
         while(j<n){
             
             mp[s[j]]++;
             
-             // maintain window size exactly k
-            if(j - i + 1 > k){
-                mp[s[i]]--;
-                if(mp[s[i]] == 0){
-                    mp.erase(s[i]);
-                }
-                i++;
-            }
-        
-            // check condition when window size == k
-            if(j - i + 1 == k){
-                if(mp.size() == k - 1){
+            if(j-i+1==k){
+                
+                if(mp.size()==k-1){
                     cnt++;
                 }
+                
+                mp[s[i]]--;
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
+                }
+                
+                i++;
             }
-        
+            
             j++;
         }
         return cnt;
