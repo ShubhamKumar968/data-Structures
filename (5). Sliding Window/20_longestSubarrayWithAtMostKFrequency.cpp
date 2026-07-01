9f701b0884a8eb4db607a358f455013b9072012c
Shubham Kumar <shubhamkgr06@gmail.com>
2026-07-01 12:30:52 +0530

    Update 20_longestSubarrayWithAtMostKFrequency.cpp

--- (5). Sliding Window/20_longestSubarrayWithAtMostKFrequency.cpp
+++ (5). Sliding Window/20_longestSubarrayWithAtMostKFrequency.cpp
@@ -7,6 +7,7 @@ public:
     int maxSubarrayLength(vector<int>& nums, int k) {
         int n=nums.size();
         unordered_map<int,int>mp;
+        
         int i=0,j=0;
         int maxLen=INT_MIN;
 
@@ -14,8 +15,11 @@ public:
             mp[nums[j]]++;
 
             while(i<j && mp[nums[j]]>k){
+                
                 mp[nums[i]]--;
-                if(mp[nums[i]]==0) mp.erase(nums[i]);
+                if(mp[nums[i]]==0) {
+                    mp.erase(nums[i]);
+                }
                 i++;
             }
 
