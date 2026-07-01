1607b04a3f7bbbefac4992a8a5728a8b77d037e8
Shubham Kumar <shubhamkgr06@gmail.com>
2026-07-01 12:14:19 +0530

    Update 18_countSubarrayWithExactlyKDistinctInteger.cpp

--- (5). Sliding Window/18_countSubarrayWithExactlyKDistinctInteger.cpp
+++ (5). Sliding Window/18_countSubarrayWithExactlyKDistinctInteger.cpp
@@ -41,3 +41,46 @@ reusable pattern for many sliding window problems.
         return countAtMostK(arr,k)- countAtMostK(arr,k-1);
     }
 };
+
+//(2) for string 
+
+class Solution {
+  public:
+    int atMost(string&s, int k) {
+        // code here
+        if (k < 0) return 0;
+        vector<int> freq(256, 0);
+        int distinct = 0;
+        int n=s.size();
+        int i=0,j=0;
+        int cnt=0;
+            
+        while (j < n) {
+            // If it's a new distinct character
+            if (freq[s[j]] == 0) {
+                distinct++;
+            }
+            
+            freq[s[j]]++;
+
+            // Shrink window if distinct characters exceed k
+            while (distinct > k) {
+                freq[s[i]]--;
+                if (freq[s[i]] == 0) {
+                    distinct--;
+                }
+                i++;
+            }
+            // Number of subarrays ending at j is the window size
+            cnt += (j - i + 1);
+            j++;
+        }
+        return cnt;
+    }
+    
+    int countSubstr(string& s, int k) {
+        // code here
+        int ans=atMost(s,k)-atMost(s,k-1);
+        return ans;
+    }
+};
