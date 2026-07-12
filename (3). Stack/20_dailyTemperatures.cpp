c02298354735d75fa849514a5dcfd296fe25e97f
Shubham Kumar <shubhamkgr06@gmail.com>
2026-07-12 10:28:40 +0530

    Update 20_dailyTemperatures.cpp

--- (3). Stack/20_dailyTemperatures.cpp
+++ (3). Stack/20_dailyTemperatures.cpp
@@ -20,24 +20,39 @@ public:
             }
         }
         return res;
-
-        //Method-2 Optimal (Using Stack)
-        int n=arr.size();
-        vector<int>res(n,0);
-        stack<int>st;
-
-        for(int i=n-1;i>=0;i--){
-            
-            while(!st.empty() && arr[st.top()]<=arr[i]){
-                st.pop();
-            }
-            res[i]=st.empty() ? 0 : st.top()-i;
-            st.push(i);
-        }
-        return res;
-
     }
 };
 
+//Method-2 Optimal (Using Stack)
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& arr) {
+
+       int n=arr.size();
+       stack<int>st;
+       vector<int>NGR(n,-1);
+       
+        for(int i=n-1;i>=0;i--){
+            
+            while(!st.empty() && arr[st.top()] <= arr[i]){
+                st.pop();
+            }
+          
+            if(!st.empty()){
+                NGR[i]=st.top();
+            }
+          
+            st.push(i);
+        }
+        
+        vector<int>temp(n);
+        for(int i=0;i<n;i++){
+           temp[i]=(NGR[i]-i) < 0 ? 0 : (NGR[i]-i);
+        }
+       
+        return temp;
+
+    }
+};
 //Here, we are finding distance to next greater element (next warmer day)
 //we are skipping the days in between if they are not warmer,So it's NOT consecutive counting.
