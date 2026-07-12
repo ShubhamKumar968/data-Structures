794df098797fd3b2fd48499def022517b6356820
Shubham Kumar <shubhamkgr06@gmail.com>
2026-07-12 10:16:27 +0530

    Update 19_stockSpanProblem.cpp

--- (3). Stack/19_stockSpanProblem.cpp
+++ (3). Stack/19_stockSpanProblem.cpp
@@ -25,24 +25,40 @@ class Solution {
             }
             span[i]=cnt;
         }
-        return span;
+      return span;
+    } 
+};
+
+//Method-2 Optimal (Using Stack)
+
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
         
-        //Method-2 Optimal (Using Stack)
-        int n=arr.size();
-        vector<int>span(n,1);
-        stack<int>st;
-
-        //In the stock span problem, the monotonic stack helps us find the previous greater element, 
-        //which determines how far the span can extend to the left.
-
+       int n=arr.size();
+       stack<int>st;
+       vector<int>NGL(n,-1);
+       
         for(int i=0;i<n;i++){
+            //In the stock span problem, the monotonic stack helps us find the previous greater element, which determines how far the span can extend to the left.
             while(!st.empty() && arr[st.top()] <= arr[i]){
                 st.pop();
             }
-            span[i]= st.empty() ? i+1 :i-st.top();
-            
+          
+            if(!st.empty()){
+                NGL[i]=st.top();
+            }
+          
             st.push(i);
         }
+        
+        // span[i]= i- NGL[i]
+        
+        vector<int>span(n);
+        for(int i=0;i<n;i++){
+           span[i]=i-NGL[i];
+        }
+       
         return span;
     }
 };
