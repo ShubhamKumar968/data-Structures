aef07b2f3a2329d03c0157543c5c55cb7737c082
Shubham Kumar <shubhamkgr06@gmail.com>
2026-07-23 08:31:18 +0530

    Update 7_numberOfIslands.cpp

--- Grid based graph Problems/7_numberOfIslands.cpp
+++ Grid based graph Problems/7_numberOfIslands.cpp
@@ -1,41 +1,69 @@
-#include<iostream>
-using namespace std;
-#include<bits/stdc++.h>
-
 class Solution {
-public:
-    vector<vector<int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+  public:
+    void bfs(vector<vector<char>>& grid,int sr,int sc,int m,int n){
+        
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        grid[sr][sc]='$';
     
-    void dfs(vector<vector<char>> &grid, int i, int j, int m, int n) {
-        // Base case: check boundaries, if already colored, or if it doesn't match original color
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j]=='0'|| grid[i][j]=='#') {
-            return;
-        }
-        
-        // Fill current cell with new color
-        grid[i][j] = '#';
-        
-        // Recursively visit all 4-directional neighbors
-        for (auto &d : dir) {
-            int ni = i + d[0];
-            int nj = j + d[1];
+        // 1. Standard BFS - expanding from the boundary "seeds" (diagonal is also direction this time)
+        while (!q.empty()) {
+            auto [x, y] = q.front(); 
+            q.pop();
             
-            dfs(grid, ni, nj, m, n);
+          for(int i=-1;i<=1;i++){
+            for(int j=-1;j<=1;j++){
+                if(i==0 && j==0) continue;
+                
+                int nx=x+i;
+                int ny=y+j;
+                
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 'L') {
+                    grid[nx][ny] = '$';
+                    q.push({nx, ny});
+                }
+            }
+         }
+            
         }
+        return;
     }
-    int numIslands(vector<vector<char>>& grid) {
-        int islands=0;
+    
+    //Standard DFS
+    void dfs(vector<vector<char>>& grid,int x,int y,int m,int n){
+        
+        if(x<0||x>=m||y<0||y>=n||grid[x][y]!='L') return;
+        
+        grid[x][y]='$';//mark visited
+        
+        for(int i=-1;i<=1;i++){
+            for(int j=-1;j<=1;j++){
+                
+                if(i==0 && j==0) continue;
+                
+                int newX=x+i;
+                int newY=y+j;
+                
+                dfs(grid,newX,newY,m,n);
+            }
+        }
+        return;
+        
+    }
+    int countIslands(vector<vector<char>>& grid) {
+        // Code here
         int m=grid.size();
         int n=grid[0].size();
+        int island=0;
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                
-                if(grid[i][j]=='1'){
-                    dfs(grid,i,j,m,n);
-                    islands++;
+                if(grid[i][j]=='L'){
+                    bfs(grid,i,j,m,n);
+                    island++;
                 }
             }
         }
-        return islands;
+        return island;
     }
 };
