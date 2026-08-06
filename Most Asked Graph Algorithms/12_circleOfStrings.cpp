b3ad00ce68959d25270379e3ea428d05a0952d94
Shubham Kumar <shubhamkgr06@gmail.com>
2026-08-06 19:39:51 +0530

    Update 12_circleOfStrings.cpp

--- Most Asked Graph Algorithms/12_circleOfStrings.cpp
+++ Most Asked Graph Algorithms/12_circleOfStrings.cpp
@@ -1,50 +1,73 @@
-#include<iostream>
-using namespace std;
-#include<bits/stdc++.h>
+class DSU {
+public:
+    vector<int> par;
+    vector<int> rank;
+
+    DSU(int n) {
+        rank.resize(n, 0);
+        par.resize(n);
+        for (int i = 0; i < n; i++) par[i] = i;
+    }
+
+    int find(int x) {
+        if (x == par[x]) return x;
+        return par[x] = find(par[x]); // Path compression
+    }
+
+    void Union(int x, int y) {
+        int x_par = find(x);
+        int y_par = find(y);
+
+        if (x_par == y_par) return;
+
+        if (rank[x_par] > rank[y_par]) {
+            par[y_par] = x_par;
+        } else if (rank[x_par] < rank[y_par]) {
+            par[x_par] = y_par;
+        } else {
+            par[y_par] = x_par;
+            rank[x_par]++;
+        }
+    }
+};
 
 class Solution {
-  public:
-   
+public:
     bool isCircle(vector<string> &arr) {
-        
-        int N = arr.size();
-        unordered_map<char, vector<char>> adj;
-        unordered_map<char, int> inDegree, outDegree;
+        DSU dsu(26);
+        vector<int> inDegree(26, 0);
+        vector<int> outDegree(26, 0);
+        vector<bool> present(26, false);
 
+        // 1. Build degree counts and unify nodes via DSU
         for (const string &s : arr) {
-            char u = s.front(), v = s.back();
-            adj[u].push_back(v);
+            int u = s.front() - 'a';
+            int v = s.back() - 'a';
+
             outDegree[u]++;
             inDegree[v]++;
+            present[u] = true;
+            present[v] = true;
+
+            dsu.Union(u, v);
         }
 
-        // Degree condition check
-        for (auto &p : adj) {
-            if (inDegree[p.first] != outDegree[p.first]) return false;
-        }
-
-        // Construct circuit using Hierholzer's Algorithm
-        stack<char> st;
-        vector<char> circuit;
-        st.push(arr[0].front());
-
-        while (!st.empty()) {
-            
-            char u = st.top();
-            
-            if (!adj[u].empty()) {
-                char v = adj[u].back();
-                adj[u].pop_back();
-                st.push(v);
-            } 
-            else {
-                circuit.push_back(u);
-                st.pop();
+        // 2. Degree condition check
+        for (int i = 0; i < 26; i++) {
+            if (inDegree[i] != outDegree[i]) {
+                return false;
             }
-            
         }
 
-        // Number of edges in an Eulerian circuit is (nodes visited in path - 1)
-        return (circuit.size() - 1) == N;
+        // 3. Collect unique representative components using unordered_set
+        unordered_set<int> components;
+        for (int i = 0; i < 26; i++) {
+            if (present[i]) {
+                components.insert(dsu.find(i));
+            }
+        }
+
+        // Exactly 1 component means all characters are part of a single connected graph
+        return components.size() == 1;
     }
 };
