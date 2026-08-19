2ab0e53d6838474f198d5d1af2191635be0efb4a
Shubham Kumar <shubhamkgr06@gmail.com>
2026-08-19 08:33:02 +0530

    Update 21_pathWithMaximumGold.cpp

--- (7). Recursion and Backtracking/21_pathWithMaximumGold.cpp
+++ (7). Recursion and Backtracking/21_pathWithMaximumGold.cpp
@@ -1,40 +1,47 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
+//TC=O(4^m*n)
 class Solution {
 public:
-//TC=O(4^m*n)
-    vector<vector<int>>dir={{-1,0},{0,-1},{1,0},{0,1}};
-    int solve(vector<vector<int>>& grid,int i, int j, int m, int n){
+    vector<vector<int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0){
+    int solve(vector<vector<int>>& grid, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
             return 0;
         }
 
-        int originalGold=grid[i][j];
-        grid[i][j]=0;//mark visited
+        int originalGold = grid[i][j];
+        grid[i][j] = 0; // Mark as visited
 
-        int maxGold=0;
-        for(auto &d: dir){
-            int ni=i+d[0];
-            int nj=j+d[1];
+        int maxNext = 0; // Find best downstream path
+        for (auto &d : dir) {
+            int ni = i + d[0];
+            int nj = j + d[1];
             
-            maxGold=max(maxGold,solve(grid,ni,nj,m,n));//explore
+            maxNext = max(maxNext, solve(grid, ni, nj, m, n));
         }
 
-        grid[i][j]=originalGold;//backtrack
-        return (maxGold+ originalGold);
+        grid[i][j] = originalGold; // Backtrack
+
+        // Add current cell's gold to the best neighboring path
+        return originalGold + maxNext;
     }
 
     int getMaximumGold(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int maxi=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]!=0)maxi=max(maxi, solve(grid,i,j,m,n));
+        int m = grid.size();
+        int n = grid[0].size();
+        int maxi = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != 0) {
+                    maxi = max(maxi, solve(grid, i, j, m, n));
+                }
             }
         }
+
         return maxi;
     }
 };
