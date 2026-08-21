257fe090c8aeb3d6248708354308f3f2a9858119
Shubham Kumar <shubhamkgr06@gmail.com>
2026-08-21 10:25:45 +0530

    Update 09_NQueensProblem.cpp

--- (7). Recursion and Backtracking/09_NQueensProblem.cpp
+++ (7). Recursion and Backtracking/09_NQueensProblem.cpp
@@ -1,64 +1,88 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
+    //Recurrence:- T(n)= n*[n+ T(n-1)] => T(n)=n*T(n-1) + n^2 ;     TC=O(n!); SC=O(n^2)
+    bool isValid(int row, int n, int col, vector<string>& board) {
 
-    vector<vector<string>>res;
-    bool isSafe(vector<string>&board, int n,int row, int col){//To check wheather queen are attacking each other or not
-         
-        //check uppward column
-        for(int i=row-1;i>=0;i--){
-            if(board[i][col]=='Q'){
+        // check same row;Since we are placing queens row by row,we only need to check the previous rows in the same column.
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q')
                 return false;
-            }
         }
-        //check left diagonal upward;(left me:- col=col-1 && row=row-1)
-        for(int i=row-1,j=col-1; i>=0 && j>=0; i--,j--){
-            if(board[i][j]=='Q'){
+
+        // check left diagonal (both row & col will decreasing)
+        for (int i = row - 1, j = col - 1;
+             i >= 0 && j >= 0;
+             i--, j--) {
+
+            if (board[i][j] == 'Q')
                 return false;
-            }
         }
-        //check right diagonal upward;(right me:- col=col+1 && row=row-1)
-        for(int i=row-1,j=col+1; i>=0 && j<n; i--,j++){
-            if(board[i][j]=='Q'){
+
+        // check right diagonal (row will decrease, col will increase)
+        for (int i = row - 1, j = col + 1;
+             i >= 0 && j < n;
+             i--, j++) {
+
+            if (board[i][j] == 'Q')
                 return false;
-            }
         }
-        
+
+        // If no queen is found in the same columnor either diagonal, this position is valid.
         return true;
     }
 
-    //Recurrence:- T(n)= n*[n+ T(n-1)] => T(n)=n*T(n-1) + n^2 ;     TC=O(n!); SC=O(n^2)
-    void solve(vector<string>&board,int row, int n){//T(n)
-        
-        if(row >= n){//All Queen are placed correctly
+
+    void solve(int row, int n, vector<string>& board,
+               vector<vector<string>>& res) {
+
+        // all queens are placed successfully, We have placed one queen in every row.
+        if (row == n) {
             res.push_back(board);
             return;
         }
 
-        for(int col=0;col<n;col++){ //O(n)
+        // Try placing the queen in every column of the current row.
+        for (int col = 0; col < n; col++) {
 
-            if(isSafe(board,n,row,col)){//O(n)
+            // Check whether placing queen at (row, col) is safe, board[row][col] == '.' ensures the cell is empty.
+            if (isValid(row, n, col, board) &&
+                board[row][col] == '.') {
 
-                board[row][col]='Q';//Do change
-                solve(board,row+1,n);//Explore => T(n-1)
-                board[row][col]='.';//Undo changes(backtrack)
+                // do
+                // Place the queen at the current position.
+                board[row][col] = 'Q';
+
+                // explore
+                // Move to the next row and try to place the remaining queens.
+                solve(row + 1, n, board, res);
+
+                // undo
+                // Remove the queen and try the next column.This is the backtracking step.
+                board[row][col] = '.';
             }
         }
 
+        return;
     }
-    vector<vector<string>> solveNQueens(int n) {
-        
-        res.clear();
-        vector<string>board(n, string(n,'.'));
-        solve(board,0,n);
 
+    vector<vector<string>> solveNQueens(int n) {
+
+        vector<vector<string>> res;
+
+        // Initially, the entire board is empty.
+        vector<string> board(n, string(n, '.'));
+
+        // Start placing queens from row 0.
+        solve(0, n, board, res);
+
+        // Return all possible valid arrangements.
         return res;
     }
 };
-
+   
 //Method-02 (optimized approach)
 
 class Solution {
@@ -111,11 +135,6 @@ public:
     }
 
     vector<vector<string>> solveNQueens(int n) {
-        
-        res.clear();
-        cols.clear();
-        diags.clear();
-        antiDiags.clear(); // important reset
         
         vector<string> board(n, string(n, '.'));
         
