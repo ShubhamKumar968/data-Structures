ef299b6671e08dc9d650389199f4bf980b8c4106
Shubham Kumar <shubhamkgr06@gmail.com>
2026-09-08 08:17:10 +0530

    Update 18_diameterOfBinaryTree.cpp

--- (9). Binary Trees and BST/BinaryTree/18_diameterOfBinaryTree.cpp
+++ (9). Binary Trees and BST/BinaryTree/18_diameterOfBinaryTree.cpp
@@ -14,36 +14,61 @@ class Node {
     }
 };
 
+//Method-01: Brute force
+
 class Solution {
   public:
     
-    //Diameter=max({leftDia, rightDia, (leftHeight + rightHight)})
-    
-    int solve(Node* root, int& res){
+    int height(Node* root){
         
-        // BASE CASE: An empty node contributes 0 to the height of a path.
         if(root==NULL){
             return 0;
         }
+         
+        int lh=height(root->left); 
+        int rh=height(root->right);
         
-        // STEP 1: Calculate the maximum height of the left and right subtrees.
-        int left=solve(root->left,res);
-        int right=solve(root->right,res);
-        
-        // STEP 2 (THE IMPORTANT DIAMETER STEP):
-        
-         //The longest path passing *through* the current node as the highest turning point 
-        //is the sum of its left height and its right height (left + right).
-        res=max(res,left+right);
-        
-        // STEP 3: Return the height of the current subtree back up to the parent node.
-        return 1+ max(left,right);
+        return 1+max(lh,rh);
     }
     
     int diameter(Node* root) {
-        // code here
-        int res=0;
-        solve(root,res);
-        return res;
+        
+        if(root==NULL) return 0;
+         // STEP 1: Calculate the maximum height of the left and right subtrees.
+        int leftH=height(root->left);
+        int rightH=height(root->right);
+        
+        int throughRoot = leftH + rightH;
+        //Diameter=max({leftDia, rightDia, (leftHeight + rightHight)})
+        int leftDia=diameter(root->left);
+        int rightDia=diameter(root->right);
+        
+        return max({throughRoot,leftDia,rightDia});
+    }
+};
+
+//Method-02: Optimal Approach
+
+class Solution {
+public:
+
+    int solve(Node* root, int &dia) {
+        if (root == NULL)
+            return 0;
+        // STEP 1: Calculate the maximum height of the left and right subtrees.
+        int lh = solve(root->left, dia);
+        int rh = solve(root->right, dia);
+
+        // STEP 2 (THE IMPORTANT DIAMETER STEP):The longest path passing *through* the current node as the highest turning point is the sum of its left height and its right height (left + right).
+       
+        dia = max(dia, lh + rh);
+        // STEP 3: Return the height of the current subtree back up to the parent node.
+        return 1 + max(lh, rh);
+    }
+
+    int diameter(Node* root) {
+        int dia = 0;
+        solve(root, dia);
+        return dia;
     }
 };
