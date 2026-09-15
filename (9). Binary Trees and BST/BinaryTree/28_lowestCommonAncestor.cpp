91fde1a0ffc1d700cab97df495decd2468797147
Shubham Kumar <shubhamkgr06@gmail.com>
2026-09-15 08:42:51 +0530

    Update 28_lowestCommonAncestor.cpp

--- (9). Binary Trees and BST/BinaryTree/28_lowestCommonAncestor.cpp
+++ (9). Binary Trees and BST/BinaryTree/28_lowestCommonAncestor.cpp
@@ -15,38 +15,78 @@ class Node {
 };
 
 
+
+//(1) Lowest Common Ancestor of a Biary Tree
 class Solution {
   public:
   
-    Node* solve(Node* root, int n1, int n2){
+    Node* solve(Node* root, int p,int q){
         
         if(root==NULL){
             return NULL;
         }
         
-        if(root->data==n1 || root->data==n2){
+        if(root->data==p || root->data==q){
             return root;
         }
         
-        Node* leftN=solve(root->left,n1,n2);
         
-        Node* rightN=solve(root->right,n1,n2);
+        Node* leftNode=solve(root->left,p,q);
+        Node* rightNode=solve(root->right,p,q);
         
-        //Agar left & right dono Null nhi hai to root hi lca hoga
-        if(leftN!=NULL && rightN !=NULL){
+        if(leftNode!=NULL && rightNode!=NULL){
             return root;
         }
-        //Otherwise jo Not Null hai wo lca hoga.
-        if(leftN==NULL){
-            return rightN;
+        else if(rightNode==NULL){
+            return leftNode;
         }
-        
-        return leftN;
+        else if(leftNode==NULL){
+            return rightNode;
+        }
         
     }
+    
     Node* lca(Node* root, int n1, int n2) {
-        //  code here
+
         return solve(root,n1,n2);
+    }
+};
+
+//(2) Kth Ancestor of a binary Tree
+
+class Solution {
+public:
+
+    bool dfs(Node* root, int node, vector<Node*>& path) {
+
+        if (!root) return false;
+
+        path.push_back(root);
+
+        if (root->data == node) return true;
         
+        bool l= dfs(root->left, node, path);
+        bool r= dfs(root->right, node, path);
+        
+        if (l ||r) return true;
+
+        path.pop_back();
+
+        return false;
+    }
+
+    int kthAncestor(Node* root, int k, int node) {
+
+        vector<Node*> path;
+
+        // Find path from root to node
+        dfs(root, node, path);
+
+        // kth ancestor index
+        int index = path.size() - k - 1;
+
+        if (index < 0) return -1;
+
+        return path[index]->data;
     }
 };
