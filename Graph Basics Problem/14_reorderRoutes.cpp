9954ee0f01adb5c0a5706628a005de33fc068e76
Shubham Kumar <shubhamkgr06@gmail.com>
2026-09-19 13:05:56 +0530

    Update 14_reorderRoutes.cpp

--- Graph Basics Problem/14_reorderRoutes.cpp
+++ Graph Basics Problem/14_reorderRoutes.cpp
@@ -31,3 +31,55 @@ public:
         return cnt;
     }
 };
+
+
+//(2) Minimum edge reversal for the path
+
+class Solution {
+public:
+    typedef pair<int, int> p;
+
+    int minimumEdgeReversal(vector<vector<int>>& edges, int V, int src, int dst){
+        
+        vector<vector<p>> adj(V+1);
+
+        // Original direction: cost 0
+        // Reverse direction: cost 1
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+
+            adj[u].push_back({v, 0});
+            adj[v].push_back({u, 1});
+        }
+
+        const int INF = 1e9;
+        vector<int> dist(V+1, INF);
+
+        deque<int> dq;
+        dist[src] = 0;
+        dq.push_front(src);
+
+        while (!dq.empty()) {
+            
+            int u = dq.front();
+            dq.pop_front();
+
+            for (auto &[v, cost] : adj[u]) {
+                
+                if (dist[u] + cost < dist[v]) {
+                    
+                    
+                    dist[v] = dist[u] + cost;
+
+                    if (cost == 0)
+                        dq.push_front(v);
+                    else
+                        dq.push_back(v);
+                }
+            }
+        }
+
+        return dist[dst]==INF ? -1: dist[dst];
+    }
+};
