5ad1071568ade2650a0239229fb985692ef42916
Shubham Kumar <shubhamkgr06@gmail.com>
2026-09-26 19:34:47 +0530

    Update 07_decodingWaysOfString.cpp

--- (10). Dynamic programming/(1). 1-D  Dp/07_decodingWaysOfString.cpp
+++ (10). Dynamic programming/(1). 1-D  Dp/07_decodingWaysOfString.cpp
@@ -2,66 +2,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-  public:
     
-    //Method-01:- Recursion && Memoization(Top Down)
+  //Method-01:- Recursion && Memoization(Top Down)
+  class Solution {
+  public:
     int t[1001];
-    int solve(string &digits,int idx,int n){
+    int solve(string &s,int i,int n){
         
-        if(idx==n){
+        if(i>=n){
             return 1;//one valid split is done
         }
         
-        if(digits[idx]=='0') return 0;//Not possible to split
+        if(t[i]!=-1) return t[i];
         
-        if(t[idx]!=-1) return t[idx];
+        int one=0;//To process one length string
         
-        int two=0;
-        if(idx+1<n){
-            if(digits[idx]=='1' || (digits[idx]=='2' && digits[idx+1]<='6')){//To process 2 length string
-                two=solve(digits,idx+2,n);
-            }
+        if(s[i]>='1' && s[i]<='9'){
+            one=solve(s,i+1,n);
         }
         
-        int one=solve(digits,idx+1,n);//To process one length string
+        int two=0;//To process 2 length string
         
-        return t[idx]=(one+two);
-    }
-    
-//Method-02.1 :- Bottom UP
-    int bottomUp(string&digits){//Using backward loop
-    
-        //dp[i]=no. of decode ways for string s from index i to n.
-        int n=digits.length();
-        vector<int>dp(n+1,0);
-        
-        dp[n]=1;//one valid split is found
-        
-        for(int i=n-1;i>=0;i--){
+        if(i+1<n && 
+             (  (s[i]=='1' && s[i+1]>='0' && s[i+1]<='9') || 
+        (s[i]=='2' && s[i+1]>='0' && s[i+1]<='6')  )  ){
             
-            if(digits[i]=='0'){
-                
-                dp[i] = 0;//Base case
-            }
-            else{
-                
-                int one=dp[i+1];//solve(i+1)
-            
-                int two=0;
-                if(i+1<n){
-                    if(digits[i]=='1' || (digits[i]=='2' && digits[i+1]<='6')){
-                        two=dp[i+2];//solve(i+2)
-                    }
-                }
-                
-                dp[i]=one+two;
-            }
-            
+            two=solve(s,i+2,n);
         }
-        return dp[0];
+        
+        return t[i]=one+two;
     }
-//Method-02.2 :- Bottom Up   
+    
+    int countWays(string &digits) {
+        
+        int n=digits.size();
+        memset(t,-1,sizeof(t));
+        return solve(digits,0,n);
+    }
+};
+
+class Solution {
+  public:
+//Method-02 :- Bottom UP
      int bottomUp_2(string&s) {//Using forward loop
         
         int n=s.length();
@@ -95,32 +77,3 @@ class Solution {
         return bottomUp_2(digits);
     }
 };
-
-
-//basic approach
-
-int t[1001];
-    int solve(string&s, int i, int n){
-        
-        if(i>=n){
-            return 1;
-        }
-        
-        if(t[i]!=-1) return t[i];
-        
-        int one=0;
-        if(s[i]>='1' && s[i]<='9'){
-            one+=solve(s,i+1,n);
-        }
-        
-        int two=0;
-        if(i+1<n){
-            if( ( s[i]=='1' && s[i+1]>='0' && s[i+1]<='9' ) ||
-               ( s[i]=='2' && s[i+1]>='0' && s[i+1]<='6' ) ){
-               
-                two+=solve(s,i+2,n);
-            }
-        }
-        
-        return t[i]=one+two;
-    }
